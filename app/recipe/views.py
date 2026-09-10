c47c71dbe5e55c29e2efbc03409a1785874484fb
mrnobody3 <demydenko.serhii1994@gmail.com>
2026-09-10 17:32:54 +0300

    feat: add tag deleting feature

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -36,7 +36,10 @@ class RecipeViewSet(viewsets.ModelViewSet):
 
 
 class TagViewSet(
-    mixins.UpdateModelMixin, mixins.ListModelMixin, viewsets.GenericViewSet
+    mixins.DestroyModelMixin,
+    mixins.UpdateModelMixin,
+    mixins.ListModelMixin,
+    viewsets.GenericViewSet,
 ):
     """View for manage tag APIs."""
 
