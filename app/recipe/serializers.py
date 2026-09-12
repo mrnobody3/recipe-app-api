28e3adc7ed01a351dc41e7c820c4f540be9052b8
mrnobody3 <demydenko.serhii1994@gmail.com>
2026-09-12 23:36:50 +0300

    feat: implement ingredient listing

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -3,7 +3,16 @@ Serializer for the recipe model.
 """
 
 from rest_framework import serializers
-from core.models import Recipe, Tag
+from core.models import Recipe, Tag, Ingredient
+
+
+class IngredientSerializer(serializers.ModelSerializer):
+    """Serializer for the Ingredient object"""
+
+    class Meta:
+        model = Ingredient
+        fields = ["id", "name"]
+        read_only_fields = ["id"]
 
 
 class TagSerializer(serializers.ModelSerializer):
@@ -19,10 +28,19 @@ class RecipeSerializer(serializers.ModelSerializer):
     """Serializer for the recipe object."""
 
     tags = TagSerializer(many=True, required=False)
+    ingredients = IngredientSerializer(many=True, required=False)
 
     class Meta:
         model = Recipe
-        fields = ("id", "title", "time_minutes", "price", "link", "tags")
+        fields = (
+            "id",
+            "title",
+            "time_minutes",
+            "price",
+            "link",
+            "tags",
+            "ingredients",
+        )
         read_only_fields = ["id"]
 
     def _get_or_create_tags(self, tags, recipe):
