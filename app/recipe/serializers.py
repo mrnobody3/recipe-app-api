419b561b745f86bfc6bdb7d2c84d6189c3456ba5
mrnobody3 <demydenko.serhii1994@gmail.com>
2026-09-17 15:24:35 +0300

    feat: add ingredient updating support for recipe editing

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -59,7 +59,6 @@ class RecipeSerializer(serializers.ModelSerializer):
                 user=auth_user, **ingredient
             )
             recipe.ingredients.add(ingredient_obj)
-        return recipe
 
     def create(self, validated_data):
         """Create a recipe"""
@@ -74,9 +73,14 @@ class RecipeSerializer(serializers.ModelSerializer):
     def update(self, instance, validated_data):
         """Update recipe"""
         tags = validated_data.pop("tags", None)
+        ingredients = validated_data.pop("ingredients", None)
         if tags is not None:
             instance.tags.clear()
             self._get_or_create_tags(tags, instance)
+
+        if ingredients is not None:
+            instance.ingredients.clear()
+            self._get_or_create_ingredients(ingredients, instance)
 
         for attr, value in validated_data.items():
             setattr(instance, attr, value)
